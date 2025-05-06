282b3213b7b2215a76093f37b1c6f4fe52220549
Safvan vp <167276814+Safvanvp@users.noreply.github.com>
2025-05-06 19:01:01 +0530

    tuesday may6

--- lib/pages/flutter_Login.dart
+++ lib/pages/flutter_Login.dart
@@ -14,53 +14,50 @@
 //     } catch (e) {
 //       return e.toString();
 //     }
-//   }
 
-//   Future<String?> onSignup(SignupData data) async {
-//     try {
-//       await _authService.signUpWithEmailPassword(
-//         data.name ?? '',
-//         data.password ?? '',
-//       );
-//       return null;
-//     } catch (e) {
-//       return e.toString();
+//       Future<String?> onSignup(SignupData data) async {
+//         try {
+//           await _authService.signUpWithEmailPassword(
+//             data.name ?? '',
+//             data.password ?? '',
+//           );
+//           return null;
+//         } catch (e) {
+//           return e.toString();
+//         }
+//       }
+    
+//       Future<String?> recoverPassword(String name) async {
+//         await Future.delayed(Duration(microseconds: 200));
+//         try {
+//           // recovery logic here
+//           return null;
+//         } catch (e) {
+//           return e.toString();
+//         }
+//       }
 //     }
-//   }
-
-//   Future<String?> recoverPassword(String name) async {
-//     await Future.delayed(Duration(microseconds: 200));
-//     try {
-//       // recovery logic here
-//       return null;
-//     } catch (e) {
-//       return e.toString();
-//     }
-//   }
-
-//   // Future<String?> onSignup(SignupData data) async {
-//   //   await Future.delayed(loadingTime).then((value) => null);
-//   // }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: FlutterLogin(
-//         onLogin: onLogin,
-//         onRecoverPassword: recoverPassword,
-//         onSignup: onSignup,
-//         hideForgotPasswordButton: true,
-//         disableCustomPageTransformer: false,
-//         theme: LoginTheme(
-//           primaryColor: Colors.teal,
-//           cardTheme: const CardTheme(
-//             color: Colors.white,
-//             shape: RoundedRectangleBorder(
-//               borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  
+//     @override
+//     Widget build(BuildContext context) {
+//       return Scaffold(
+//         body: FlutterLogin(
+//           onLogin: onLogin,
+//           onRecoverPassword: recoverPassword,
+//           onSignup: onSignup,
+//           hideForgotPasswordButton: true,
+//           disableCustomPageTransformer: false,
+//           theme: LoginTheme(
+//             primaryColor: Colors.teal,
+//             cardTheme: const CardTheme(
+//               color: Colors.white,
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.all(Radius.circular(15.0)),
+//               ),
 //             ),
 //           ),
 //         ),
-//       ),
-//     );
+//       );
+//     }
 //   }
 // }
